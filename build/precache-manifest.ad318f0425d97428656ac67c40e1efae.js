bdb89fff1f6e4749521cbd90b487a2b1716d0745
krebikshaw <krebikshaw@gmail.com>
2021-01-02 01:49:57 +0800

    build: add deploy file

---
+++ build/precache-manifest.ad318f0425d97428656ac67c40e1efae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dddbf95a038bc116fd2b30a41f675c1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0da4c75e7b0d3928b3da",
+    "url": "/portfolio/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "552e67c02c3e34abef16",
+    "url": "/portfolio/static/css/main.6902a0b6.chunk.css"
+  },
+  {
+    "revision": "0da4c75e7b0d3928b3da",
+    "url": "/portfolio/static/js/2.1a3e3a6c.chunk.js"
+  },
+  {
+    "revision": "552e67c02c3e34abef16",
+    "url": "/portfolio/static/js/main.97879ef4.chunk.js"
+  },
+  {
+    "revision": "b3df26dbe838babbe36b",
+    "url": "/portfolio/static/js/runtime~main.18bca874.js"
+  }
+]);
