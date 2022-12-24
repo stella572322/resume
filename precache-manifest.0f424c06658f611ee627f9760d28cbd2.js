7674076fb76b74548e943dbe3040a875067067b3
蘇裕翔 <xiang@25sprout.com>
2022-12-24 16:36:22 +0800

    Updates

---
+++ precache-manifest.0f424c06658f611ee627f9760d28cbd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0fe8b6e271ef02e3a82dda0556421a",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "75ecff98bcb031323d29",
+    "url": "/resume/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "4aaa45ec44cce7fc62fe",
+    "url": "/resume/static/css/main.e87da209.chunk.css"
+  },
+  {
+    "revision": "75ecff98bcb031323d29",
+    "url": "/resume/static/js/2.a2a26857.chunk.js"
+  },
+  {
+    "revision": "4aaa45ec44cce7fc62fe",
+    "url": "/resume/static/js/main.91a36f20.chunk.js"
+  },
+  {
+    "revision": "d774d29e746899c1691e",
+    "url": "/resume/static/js/runtime~main.bdd37323.js"
+  }
+]);
