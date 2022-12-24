494f80bac3e5f78be2e6009f649d1f211ac79f39
蘇裕翔 <xiang@25sprout.com>
2022-12-24 16:32:48 +0800

    chore: Init the project

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -4,15 +4,15 @@ const ContactList = () => (
   <div className="contact-info">
     <p>
       <i className="fas fa-paper-plane fa-lg"></i>
-      <a href="mailto:krebikshaw@gmail.com" target="_blank" rel="noopener noreferrer">krebikshaw@gmail.com</a>
+      <a href="mailto:yuxiangsu0220@gmail.com" target="_blank" rel="noopener noreferrer">yuxiangsu0220@gmail.com</a>
     </p>
     <p>
       <i className="fas fa-phone-alt fa-lg"></i>
-      <a href="tel:+996-911-277-811">+886 911 277 811</a>
+      <a href="tel:+996-911-277-811">+886 980 517 778</a>
     </p>
     <p className="resume">
       <i className="far fa-file-alt fa-lg"></i>
-      <a href="./YuXiang_Profolio.pdf" download="YuXiang_Resume">resume (pdf)</a>
+      <a href="./YuXiang_Profolio.pdf" download="SsuChieh_Resume">resume (pdf)</a>
     </p>
   </div>
 );
--- src/components/introduce/Introduce.jsx
+++ src/components/introduce/Introduce.jsx
@@ -3,13 +3,13 @@ import ContactList from './../contactList/ContactList';
 
 const Introduce = () => (
   <section className="section no-border no-margin">
-    <h1 className="name-title">YUXIANG SU</h1>
+    <h1 className="name-title">Ssu Chieh Chen</h1>
     <section className="introduce">
-      <p><strong>嗨！ 我是 裕翔，一個還在自我精進的 Front-End Developer 🎅</strong></p>
+      <p><strong>嗨！ 我是 思潔，一個還在自我精進的 UI/UX Designer 🎅</strong></p>
       <p>
         Taipei <br />
-        在前端的世界中持續尋寶，透過規劃構思出藍圖，並藉由程式去建築自己的想法。<br />
-        參與 2020 程式導師計畫，從中獲取開啟前端大門的鑰匙
+        在 UI/UX 的世界中持續尋寶，透過規劃構思出藍圖，並藉由程式去建築自己的想法。<br />
+        參與 2022 UXY 計畫，從中獲取開啟 UI/UX 大門的鑰匙
         </p>
     </section>
     <ContactList />
