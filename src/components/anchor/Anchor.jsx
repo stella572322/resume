23cd96f13dbf578e39f551d9c9398f2c712bac5d
stella572322 <yuxiangsu0220@gmail.com>
2023-02-05 01:16:21 +0800

    fix: Correct the Anchor behavior

--- src/components/anchor/Anchor.jsx
+++ src/components/anchor/Anchor.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { Anchor } from 'antd';
 const { Link } = Anchor;
 
-const AnchorMenu = () => (
+const AnchorMenu = () => {
+  const preventClick = e => {
+    e.preventDefault();
+  };
 
-  <Anchor offsetTop={50} targetOffset={60}>
-    <Link href="#skill" title="Skill" />
-    <Link href="#project" title="Project" />
-    <Link href="#work" title="Experience" />
-    <Link href="#education" title="Education" />
-    <Link href="#about" title="About Me" />
-    <Link href="#contact" title="Contact" />
-  </Anchor >
-);
+  return (
+    <Anchor offsetTop={50} targetOffset={60} onClick={preventClick}>
+      <Link href="#skill" title="Skill" />
+      <Link href="#project" title="Project" />
+      <Link href="#work" title="Experience" />
+      <Link href="#education" title="Education" />
+      <Link href="#about" title="About Me" />
+      <Link href="#contact" title="Contact" />
+    </Anchor >
+  );
+};
 
 export default AnchorMenu;
