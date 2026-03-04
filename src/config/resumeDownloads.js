c09d38426194eba830809378c7e14af6b6bde791
蘇裕翔 <xiang@25sprout.com>
2026-03-04 10:54:40 +0800

    feat: Add pdf file

--- src/config/resumeDownloads.js
+++ src/config/resumeDownloads.js
@@ -1,10 +1,10 @@
 export const RESUME_DOWNLOADS = {
   zh: {
-    url: '',
-    filename: 'resume-zh.pdf',
+    url: 'https://raw.githubusercontent.com/stella572322/resume/master/public/Resume.pdf',
+    filename: 'Stella_Resume.pdf',
   },
   en: {
-    url: '',
-    filename: 'resume-en.pdf',
+    url: 'https://raw.githubusercontent.com/stella572322/resume/master/public/Resume_En.pdf',
+    filename: 'Stella_Resume_En.pdf',
   },
 };
